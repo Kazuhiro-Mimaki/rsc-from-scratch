a92e7deb46e9e237c193320cc66daea932778f2b
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2023-07-07 08:49:03 +0900

    challenge-5: setup

---
+++ challenge-5/server/rsc.js
@@ -0,0 +1,191 @@
+import { createServer } from "http";
+import { readFile, readdir } from "fs/promises";
+import sanitizeFilename from "sanitize-filename";
+import ReactMarkdown from "react-markdown";
+import sizeOf from "image-size";
+
+createServer(async (req, res) => {
+  try {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    await sendJSX(res, <Router url={url} />);
+  } catch (err) {
+    console.error(err);
+    res.statusCode = err.statusCode ?? 500;
+    res.end();
+  }
+}).listen(8081);
+
+function Router({ url }) {
+  let page;
+  if (url.pathname === "/") {
+    page = <BlogIndexPage />;
+  } else {
+    const postSlug = sanitizeFilename(url.pathname.slice(1));
+    page = <BlogPostPage postSlug={postSlug} />;
+  }
+  return <BlogLayout>{page}</BlogLayout>;
+}
+
+async function BlogIndexPage() {
+  const postFiles = await readdir("./posts");
+  const postSlugs = postFiles.map((file) =>
+    file.slice(0, file.lastIndexOf("."))
+  );
+  return (
+    <section>
+      <h1>Welcome to my blog</h1>
+      <div>
+        {postSlugs.map((slug) => (
+          <Post key={slug} slug={slug} />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+function BlogPostPage({ postSlug }) {
+  return <Post slug={postSlug} />;
+}
+
+async function Post({ slug }) {
+  let content;
+  try {
+    content = await readFile("./posts/" + slug + ".md", "utf8");
+  } catch (err) {
+    throwNotFound(err);
+  }
+  return (
+    <section>
+      <h2>
+        <a href={"/" + slug}>{slug}</a>
+      </h2>
+      <ReactMarkdown components={{ img: Image }}>{content}</ReactMarkdown>
+    </section>
+  );
+}
+
+function Image({ src, alt }) {
+  const img = sizeOf(`./static/images/${src}`);
+  const { width, height } = {
+    width: img.height / 20,
+    height: img.width / 20,
+  };
+  return <img src={`/images/${src}`} alt={alt} width={width} height={height} />;
+}
+
+function BlogLayout({ children }) {
+  const author = "Jae Doe";
+  return (
+    <html>
+      <head>
+        <title>My blog</title>
+      </head>
+      <body>
+        <nav>
+          <a href="/">Home</a>
+          <hr />
+          <input />
+          <hr />
+        </nav>
+        <main>{children}</main>
+        <Footer author={author} />
+      </body>
+    </html>
+  );
+}
+
+function Footer({ author }) {
+  return (
+    <footer>
+      <hr />
+      <p>
+        <i>
+          (c) {author} {new Date().getFullYear()}
+        </i>
+      </p>
+    </footer>
+  );
+}
+
+async function sendJSX(res, jsx) {
+  const clientJSX = await renderJSXToClientJSX(jsx);
+  const clientJSXString = JSON.stringify(clientJSX, stringifyJSX);
+  res.setHeader("Content-Type", "application/json");
+  res.end(clientJSXString);
+}
+
+function throwNotFound(cause) {
+  const notFound = new Error("Not found.", { cause });
+  notFound.statusCode = 404;
+  throw notFound;
+}
+
+function stringifyJSX(key, value) {
+  if (value === Symbol.for("react.element")) {
+    return "$RE";
+  } else if (typeof value === "string" && value.startsWith("$")) {
+    return "$" + value;
+  } else {
+    return value;
+  }
+}
+
+async function renderJSXToClientJSX(jsx) {
+  if (
+    typeof jsx === "string" ||
+    typeof jsx === "number" ||
+    typeof jsx === "boolean" ||
+    jsx == null
+  ) {
+    /**
+     * Don't need to do anything special with these types.
+     */
+    return jsx;
+  } else if (Array.isArray(jsx)) {
+    /**
+     * Process each item in an array.
+     */
+    return Promise.all(jsx.map((child) => renderJSXToClientJSX(child)));
+  } else if (jsx != null && typeof jsx === "object") {
+    if (jsx.$$typeof === Symbol.for("react.element")) {
+      if (typeof jsx.type === "string") {
+        /**
+         * This is a component like <div />.
+         * Go over its props to make sure they can be turned into JSON.
+         */
+        return {
+          ...jsx,
+          props: await renderJSXToClientJSX(jsx.props),
+        };
+      } else if (jsx.type === Symbol.for("react.fragment")) {
+        /**
+         * This is a fragment like <>...</>.
+         * Go over its children to make sure they can be turned into JSON.
+         */
+        return await renderJSXToClientJSX(jsx.props.children);
+      } else if (typeof jsx.type === "function") {
+        /**
+         * This is a custom React component (like <Footer />).
+         * Call its function, and repeat the procedure for the JSX it returns.
+         */
+        const Component = jsx.type;
+        const props = jsx.props;
+        const returnedJsx = await Component(props);
+        return renderJSXToClientJSX(returnedJsx);
+      } else throw new Error("Not implemented.");
+    } else {
+      /**
+       * This is an arbitrary object (for example, props, or something inside of them).
+       * Go over every value inside, and process it too in case there's some JSX in it.
+       */
+      return Object.fromEntries(
+        await Promise.all(
+          Object.entries(jsx).map(async ([propName, value]) => [
+            propName,
+            await renderJSXToClientJSX(value),
+          ])
+        )
+      );
+    }
+  } else throw new Error("Not implemented");
+}
